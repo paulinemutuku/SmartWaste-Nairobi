08a7e326ef18f858ab0edb9802ab676e0287a2e1
Pauline <p.mutuku@alustudent.com>
2025-10-25 15:14:33 +0300

    Add project files without node_modules

--- SmartWaste_Web/backend/routes/schedule.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { Schedule, getSchedule } = require("../controller/scheduleController");
-
-const router = express.Router();
-
-router.post("/scheduleCollection", Schedule);
-router.get("/scheduleCollection", getSchedule);
-module.exports = router;
