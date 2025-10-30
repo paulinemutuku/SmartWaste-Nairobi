4d5e0dcb7574f3f57e8c5c549222d6a4d1ccc2d8
Pauline <p.mutuku@alustudent.com>
2025-10-30 22:56:56 +0300

    Updates

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use('/api/schedules', require('./routes/schedules'));
 app.use('/api/feedback', feedbackRoutes);
 app.use('/uploads', express.static('uploads'));
 
-const MONGODB_URI = 'mongodb+srv://smartwaste-admin:Starlin3.@cluster0.bqz6d8a.mongodb.net/smartwaste?retryWrites=true&w=majority';
+const MONGODB_URI = 'mongodb+srv://smartwaste:SmartWaste123@cluster0.bqz6d8a.mongodb.net/smartwaste?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('💾 Connected to MongoDB'))
   .catch(err => console.log('❌ MongoDB connection error:', err));
