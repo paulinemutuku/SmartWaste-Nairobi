34d0c1bb948d3e30fcabcdd59891a60abaa2f2fa
Pauline <p.mutuku@alustudent.com>
2025-11-07 09:03:35 +0300

    Updates

--- web/frontend/src/pages/Dashboard.jsx
+++ web/frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "../components/Dashboard/Dashboard.css";
-import Header from "../components/Dashboard/Header";
 import Sidebar from "../components/Dashboard/Sidebar";
 import Home from "../components/Dashboard/Home";
 
 function Dashboard() {
   return (
     <div className="grid-container">
-      <Header />
       <Sidebar />
       <Home />
     </div>
