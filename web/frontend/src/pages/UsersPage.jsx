86d5e88ff5edcbf8d2c4dd4287ff1bee5e34a711
Pauline <p.mutuku@alustudent.com>
2025-11-07 09:11:10 +0300

    Updates

--- web/frontend/src/pages/UsersPage.jsx
+++ web/frontend/src/pages/UsersPage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "../components/Dashboard/Dashboard.css";
-import Header from "../components/Dashboard/Header";
 import Sidebar from "../components/Dashboard/Sidebar";
 import Users from "./Users";
 
 function UsersPage() {
   return (
     <div className="grid-container">
-      <Header />
       <Sidebar />
       <Users />
     </div>
